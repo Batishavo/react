21d58f92d6fd77b231c2e093aa0ef380aa52d260
Batishavo <romero.martinez.carlosraul@gmail.com>
2021-09-21 17:23:53 -0500

    Secion2

---
+++ 02-proyecto1Controlador/components/Contador.js
@@ -0,0 +1,16 @@
+const Contador = () =>{
+    const [contador,setContador] =React.useState(0);
+   // console.log(contador);
+    const aumentar=()=> setContador(contador + 1);
+    const disminuir=()=> setContador(contador - 1);
+
+    return( 
+        <div>
+            <h1 className={contador<0? "menor":"mayor"}>Contador: {contador} </h1>
+            <hr/>
+
+            <button  onClick={aumentar} >Aumentar</button>
+            <button onClick={disminuir} >Disminuir</button>   
+        </div>
+    );
+};
